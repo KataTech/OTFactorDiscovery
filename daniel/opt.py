b1feefaa3ec7afd85b0aa02f7e1d048524ffd2d2
waniel-dang <78218342+waniel-dang@users.noreply.github.com>
2023-06-08 14:12:27 -0400

    achieve working plot and introduce choice of algorithm

--- daniel/opt.py
+++ daniel/opt.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-class SaddlePointOptimizer:
+class SaddlePoint:
     """
     func MUST be a Sympy expression of x and y.
     """
@@ -22,7 +22,7 @@ class SaddlePointOptimizer:
         # Store the gradients as a column vector
         self.grad = sp.Matrix([[self.grad_x], [self.grad_y]])
 
-    def optimize(self, x_init, y_init, eta=0.1, max_steps=1e4, tolerance=1e-5, make_plot=True):
+    def optimize(self, x_init, y_init, algo="gradient_descent", eta=0.1, max_steps=1e4, tolerance=1e-5):
         # Initialize the starting point as a NumPy array
         z = np.array([[x_init], [y_init]], dtype=float)
         # Define the matrix J
@@ -38,8 +38,20 @@ class SaddlePointOptimizer:
             # Check if the norm of the gradient is below the tolerance
             if np.linalg.norm(G) < tolerance:
                 break
-            # Update z using the optimization step formula
-            z -= eta * np.matmul(J, G)
+            # Update z based on the selected optimization algorithm
+            if algo == "gradient_descent":
+                z -= eta * np.matmul(J, G)
+            elif algo == "twisted_gradient_descent":
+                # Add code for Newton's method update
+                # Update z using Newton's method formula
+                pass
+            elif algo == "other_algorithm":
+                # Add code for another optimization algorithm
+                # Update z using the other algorithm's update formula
+                pass
+            else:
+                raise ValueError("Invalid algorithm choice.")
+            
             # Append the updated z value to the list
             z_values[:, now_step] = z.flatten()
             now_step += 1
@@ -47,36 +59,33 @@ class SaddlePointOptimizer:
         # Convert the list of z values to a NumPy array
         self.history = z_values[:, :now_step]
 
-        if make_plot:
-            self.plot()
 
-    def plot(self):
+    def plot(self, pitch, yaw):
+        reach = np.max(np.abs(self.history))
+        # Extract the x and y values from the optimization history
         x_values = self.history[0, :]
         y_values = self.history[1, :]
         x, y = sp.symbols('x y', real=True)
-        # Define the range of x and y values for plotting
-        x_range = np.linspace(min)
-        y_range = np.linspace(-10, 10, 100)
-        # Create a meshgrid from the x and y ranges
-        x_mesh, y_mesh = np.meshgrid(x_range, y_range)
+        
         # Create a callable function for the expression self.L
         func_callable = sp.lambdify((x, y), self.L, modules='numpy')
+
+        # Define the range of x and y values for plotting based on data
+        x_range = np.linspace(-1.1*reach, 1.1*reach, 100)
+        y_range = np.linspace(-1.1*reach, 1.1*reach, 100)
+        
+        # Create a meshgrid from the x and y ranges
+        x_mesh, y_mesh = np.meshgrid(x_range, y_range)
+        
         # Evaluate the function at each point of the meshgrid
-        z_mesh = func_callable(x_mesh, y_mesh)
+        f_mesh = func_callable(x_mesh, y_mesh)
 
         # Create a 3D plot
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
+        
         # Plot the function surface
-        ax.plot_surface(x_mesh, y_mesh, z_mesh, cmap='viridis')
-
-        # Extract the x, y, and z values from the optimization history
-        z_values_x = self.history[:, 0]
-        z_values_y = self.history[:, 1]
-        z_values_z = np.array([self.L.subs({x: x_val, y: y_val}).evalf() for x_val, y_val in zip(z_values_x, z_values_y)])
-
-        # Plot the path traced by z during optimization
-        ax.plot(z_values_x, z_values_y, z_values_z, 'r.-')
+        ax.plot_surface(x_mesh, y_mesh, f_mesh, cmap='viridis')
 
         # Set labels and title
         ax.set_xlabel('x')
@@ -84,9 +93,20 @@ class SaddlePointOptimizer:
         ax.set_zlabel('L')
         ax.set_title('Optimization Process')
 
-        # Enable mouse rotation
-        ax.mouse_init()
+        # Set the bird's eye view (top-down view)
+        ax.view_init(elev=pitch, azim=yaw)
+
+        # Extract the x, y, and z values from the optimization history
+        f_values = func_callable(x_values, y_values)
+
+        # Plot the path traced by z during optimization
+        ax.plot(x_values, y_values, f_values, color='r', marker='o', zorder=10)
+
+        # Adjust spacing to prevent overlapping
+        plt.tight_layout()
+
+        # Enable interactive mode
+        plt.ion()
 
         # Show the plot
         plt.show()
-
