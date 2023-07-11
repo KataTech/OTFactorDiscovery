a6ca99bd55a26146a93ab6a4b134bc87ae13907d
KataTech <kmh9@rice.edu>
2023-07-11 11:41:31 -0400

    Initialized SSOT class and pre-training methods

--- kai/src/flow_ot.py
+++ kai/src/flow_ot.py
@@ -131,6 +131,12 @@ def compute_barycenter(x, z, y_init, lam, barycenter_cost_grad=gaussian_kernel_k
         print("Number of iterations = {}".format(iter))
     return y
 
+def gaussian_kernel_single(x_i, x_l, sigma): 
+    """
+    Computes a single entry of the gaussian kernel
+    """
+    return np.exp(np.linalg.norm(x_i - x_l) / (-2 * sigma ** 2))
+
 def gaussian_kernel_duo(Y, Y_center, sigma = 1.0): 
     """
     Compute the kernel matrix using Y and Y_center
@@ -170,3 +176,64 @@ def kl_barycenter_loss(Y, Y_center, X, Z, lam, kern_y = gaussian_kernel_duo, ker
             print(f"The loss value evaluation for entry i = {i}: {temp}")
         loss += temp 
     return loss
+
+class SemiSupervisedOT(): 
+    """
+    A class dedicated to solving the semi-supervised OT problem
+    """
+    def __init__(self, kernel_y = "gaussian", kernel_z = "gaussian", kernel_y_bandwidth = (1.0), 
+                 kernel_z_bandwidth = (1.0), regularizer = "kl_divergence"): 
+        """
+        Initialize the setting of the solver. The default solver uses gaussian kernels with sigma = 1.0 
+        for both the transported observations, y, and the hidden factors, z. 
+
+        Inputs: 
+            kernel_y: the kernel to use for the transported observations, y. 
+            kernel_z: the kernel to use for the latent factors, z. 
+            kernel_y_bandwidth: the parameters for the y kernel. 
+            kernel_z_bandwidth: the parameters for the z kernel. 
+            regularizer: the type of regularizer to use for enforcing the independence condition. 
+        """
+        self.kern_y_params = kernel_y_bandwidth
+        self.kern_z_params = kernel_z_bandwidth
+        self.regularizer = regularizer
+        self.kernel_y = kernel_y
+        if kernel_z == "gaussian": 
+            self.kernel_z = gaussian_kernel_single
+        else: 
+            raise NotImplementedError
+        
+    
+    def initialize(self, X: np.ndarray, Z: np.ndarray, K: int, labels: np.ndarray): 
+        """
+        Pre-compute attributes based on the supplied training data. 
+
+        Inputs: 
+            X: a N-by-M numpy array of observations where each row represents an observation
+                for a total of N observations with M attributes each. 
+            Z: a K-by-1 vector indicating the value associated with the K-th class. 
+            K: the total number of classes. 
+            labels: a N-by-1 numpy array of labels. If an entry is -1, then that means the label
+                    is unknown. Otherwise, it should be an integer between 0 and K - 1, inclusive. 
+        """
+        # set global dimensional variables
+        self.N, self.M = X.shape
+        self.K = K
+        # conduct sanity checks for the dimensions of the input variables
+        assert labels.shape[0] == self.N, "ERROR: The number of entries in labels does not match N."
+        # initialize the probability matrix P, where the entry (i, k) represents the probability 
+        # that the i-th observation belongs to class k
+        self.P = np.zeros((self.N, self.K))
+        for i in range(self.N): 
+            if labels[i] == -1: 
+                # assign uniform probability to each class if the observation class is unknown
+                self.P[i, :] = 1 / float(self.K)
+            else: 
+                self.P[i, labels[i]] = 1.0
+        # pre-compute the kernel with respect to each class
+        self.kern_z = np.zeros((self.K, self.K))
+        for k in range(self.K): 
+            for kk in range(self.K): 
+                self.kern_z[k, kk] = self.kernel_z(Z[k], Z[kk], *self.kern_z_params)
+    
+                
