a614536ef4c6397eea9fff9c4dfbe430fac5769a
KataTech <kmh9@rice.edu>
2023-07-23 15:44:58 -0400

    Added hyperparameter maps and changed file directory to be global

--- kai/constants.py
+++ kai/constants.py
@@ -1,28 +1,29 @@
-DATA_PATH = "kai/data"
-OUTPUT_PATH = "kai/output"
-AMR_UTI_PATH = "kai/data/amr-uti-antimicrobial-resistance-in-urinary-tract-infections-1.0.0"
+DATA_PATH = "/Users/kaihung/optimalEnsemble/OTFactorDiscovery/kai/data"
+OUTPUT_PATH = "/Users/kaihung/optimalEnsemble/OTFactorDiscovery/kai/outputs"
+AMR_UTI_PATH = "/Users/kaihung/optimalEnsemble/OTFactorDiscovery/kai/data/amr-uti-antimicrobial-resistance-in-urinary-tract-infections-1.0.0"
 
 hyperparams = {
     "seed": 125, 
     "sigma_y": 1,
     "sigma_z": 1,
-    "lr": 0.001,
+    "lr": 0.005,
     "epsilon": 0.001,
-    "max_iter": 2000,
+    "max_iter": 4000,
     "growing_lambda": True,
     "init_lam": 0,
     "warm_stop": 500,
-    "max_lam": 1000,
+    "max_lam": 250,
     "mock_prob": False,
     "eta": 0.01,
     "verbose": True,
-    "monitoring_skip": 5
+    "monitoring_skip": 1,
+    "monitoring_skip_2": 5
 }
 
 gauss_params = {
-    "mu_A": -1,
+    "mu_A": -2,
     "sigma_A": 1,
-    "mu_B": 1,
+    "mu_B": 2,
     "sigma_B": 1,
-    "num_samples": 100
+    "num_samples": 200
 }
