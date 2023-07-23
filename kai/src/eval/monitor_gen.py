e2599a0f6fc28a4fd4a7668f342941dc87c61481
KataTech <kmh9@rice.edu>
2023-07-23 15:43:30 -0400

    Implemented monitor generators for barycenter fit check and KDE estimation of samples across training

--- kai/src/eval/monitor_gen.py
+++ kai/src/eval/monitor_gen.py
@@ -2,6 +2,8 @@
 A collection of monitor generators.
 """
 import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
 import scipy.stats as stats
 from ..model.monitor import Monitor
 
@@ -18,7 +20,7 @@ class Monitor_Gen():
         self.reporter_args = reporter_args
         self.tracker_args = tracker_args
 
-    def tracker(self, state_trackers, model, params, *args): 
+    def tracker(self, state_tracker, model, params, *args): 
         return
 
     def reporter(self, model, params, *args): 
@@ -39,7 +41,7 @@ class Barycenter_Fit_Gen(Monitor_Gen):
     two groups of observations. 
     """
 
-    def tracker(self, state_trackers, model, params, *args): 
+    def tracker(self, state_tracker, model, params, *args): 
         """
         Tracks the internal state of the model 
         relevant to the barycenter fit. 
@@ -52,8 +54,8 @@ class Barycenter_Fit_Gen(Monitor_Gen):
                             between the distribution of each class. 
         """
         # track the current regularizer value 
-        state_trackers["Iteration"].append(params["iteration"])
-        state_trackers["Lambda"].append(params["lam"])
+        state_tracker["Iteration"].append(params["iteration"])
+        state_tracker["Lambda"].append(params["lam"])
         # tracl the current y points per group 
         Y_compact = model.select_best(params["Y"], params["mock_prob"])[0]
         y_a = Y_compact[params["label"] == 0]
@@ -61,11 +63,39 @@ class Barycenter_Fit_Gen(Monitor_Gen):
         # compute the kl divergence 
         p_a = np.histogram(y_a)[0] / len(y_a)
         p_b = np.histogram(y_b)[0] / len(y_b)
-        state_trackers["KL"].append(stats.entropy(p_a, p_b))
+        state_tracker["KL"].append(stats.entropy(p_a, p_b))
         # track the current hypothesis test result 
-        truth_sample = state_trackers["GROUND_TRUTH"]
-        state_trackers["P_VALUE"].append(stats.ks_2samp(truth_sample, Y_compact.flatten())[1])
+        truth_sample = state_tracker["GROUND_TRUTH"]
+        state_tracker["P_VALUE"].append(stats.ks_2samp(truth_sample, Y_compact.flatten())[1])
         return
 
     def reporter(self, model, params, *args): 
-        return
+        return super().reporter(model, params, *args)
+
+class Gaussian_Vis_Gen(Monitor_Gen): 
+    """
+    Generates a monitor that tracks the kernel density estimation plot 
+    of the barycenter distributions as the training procedure progresses. 
+    """
+    def tracker(self, state_tracker, model, params, *args): 
+        """
+        Tracks the internal state of the model 
+        relevant to the gaussian visualizations. 
+
+        Inputs: 
+            self: the monitor object itself.
+            state_tracker: a dictionary tracking relevant states 
+                            of the model. In this case, it should 
+                            be a dictionary containing kdeplot 
+                            axes of each iteration. 
+        """
+        Y_compact = model.select_best(params["Y"], params["mock_prob"])[0]
+        y_a = Y_compact[params["label"] == 0]
+        y_b = Y_compact[params["label"] == 1]
+        state_tracker["Iteration"].append(params["iteration"])
+        state_tracker["SAMPLE_A"].append(y_a)
+        state_tracker["SAMPLE_B"].append(y_b)
+        return 
+
+    def reporter(self, model, params, *args):
+        return super().reporter(model, params, *args)
--- kai/src/eval/vis.py
+++ kai/src/eval/vis.py
@@ -7,26 +7,44 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 
 def plot_two_curves(y1, y2, x, title, left_axis_name, right_axis_name, x_axis_name, 
-                    save = False, save_path = None): 
+                    save = False, save_path = None, y3 = None, y3_axis_name = None): 
     """
     Plot two curves on the same plot. The scales should be different so that 
     both curves are visible on the screen with different colors,
     """
-    fig, ax1 = plt.subplots()
-    ax1.plot(x, y1, 'b-')
-    ax1.set_xlabel(x_axis_name)
-    # Make the y-axis label, ticks and tick labels match the line color.
-    ax1.set_ylabel(left_axis_name, color='b')
-    ax1.tick_params('y', colors='b')
+    if y3 is None: 
+        fig, ax1 = plt.subplots()
+        ax1.plot(x, y1, 'b-')
+        ax1.set_xlabel(x_axis_name)
+        # Make the y-axis label, ticks and tick labels match the line color.
+        ax1.set_ylabel(left_axis_name, color='b')
+        ax1.tick_params('y', colors='b')
 
-    ax2 = ax1.twinx()
-    ax2.plot(x, y2, 'r-')
-    ax2.set_ylabel(right_axis_name, color='r')
-    ax2.tick_params('y', colors='r')
+        ax2 = ax1.twinx()
+        ax2.plot(x, y2, 'r-')
+        ax2.set_ylabel(right_axis_name, color='r')
+        ax2.tick_params('y', colors='r')
 
-    fig.tight_layout()
+    # if the third curve is provided, plot it in a figure 
+    # below the figure of the first two curves, but share the same x-axis
+    else: 
+        fig, axs = plt.subplots(2, 1, sharex = True, height_ratios = [3, 1])
+        axs[0].plot(x, y1, 'b-')
+        axs[0].set_ylabel(left_axis_name, color='b')
+        axs[0].tick_params('y', colors='b')
+        ax2 = axs[0].twinx()
+        ax2.plot(x, y2, 'r-')
+        ax2.set_ylabel(right_axis_name, color='r')
+        ax2.tick_params('y', colors='r')
+
+        axs[1].plot(x, y3, 'g-')
+        axs[1].set_ylabel(y3_axis_name, color='g')
+        axs[1].tick_params('y', colors='g')
+
     plt.title(title)
     if save: 
         plt.savefig(save_path)
     else: 
         plt.show()
+    
+    
