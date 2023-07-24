845713beb18386abd07e03f6c1b2e7e9a6d053b2
waniel-dang <78218342+waniel-dang@users.noreply.github.com>
2023-07-24 15:32:30 -0400

    add method to produce Voronoi boundaries; improve the initialization study, including tables and additional ++ distance; begin partially implementing Lp with non-integer p

--- daniel/Clustering/utils.py
+++ daniel/Clustering/utils.py
@@ -1,6 +1,9 @@
 """
 Author: Daniel Wang
 Date: 2023-06-23
+
+NOTE: THIS MODULE IS DEPRECATED. The functions in the script are private functions that were previously called from kgencenters.py.
+The functions have been moved to the kgencenters.py script and are now called from there. This script is no longer used.
 """
 
 import numpy as np
@@ -22,20 +25,20 @@ def is_euclidean_power(cost_metric):
     return re.match(pattern, cost_metric) is not None
 
 
-def update_optimoids(X, labels, n_clusters, optimoids, cost_metric=None, tolerance=None, max_steps=None, descent_rate=None, max_descents=None):
+def update_centers(X, labels, n_clusters, centers, cost_metric=None, tolerance=None, max_steps=None, descent_rate=None, max_descents=None):
     """
-    This function updates the optimoids based on the given cost metric.
+    This function updates the centers based on the given cost metric.
     
     :param X: The input data.
     :param labels: The cluster labels for the data.
     :param n_clusters: The number of clusters.
-    :param optimoids: The current optimoids.
+    :param centers: The current centers.
     :param cost_metric: The cost metric used for clustering. 
     :param tolerance: The tolerance for convergence.
     :param max_steps: The maximum number of steps for the Weiszfeld algorithm.
     :param descent_rate: The learning rate for gradient descent.
     :param max_descents: The maximum number of descents for gradient descent.
-    :return: The updated optimoids.
+    :return: The updated centers.
     """
     if cost_metric is None:
         raise ValueError('No value for the argument "cost_metric" was received.')
@@ -48,23 +51,25 @@ def update_optimoids(X, labels, n_clusters, optimoids, cost_metric=None, toleran
     if max_descents is None:
         raise ValueError('No value for the argument "max_descents" was received.')
     
-    optimoids = np.copy(optimoids)
+    centers = np.copy(centers)
     if cost_metric in ['squared_euclidean', 'euclidean^2']:
         for k in range(n_clusters):
             cluster_points = X[labels == k]
             if len(cluster_points) > 0:
-                optimoids[k] = np.mean(cluster_points, axis=0)
+                centers[k] = np.mean(cluster_points, axis=0)
     elif cost_metric in ['manhattan', 'L1']:
         for k in range(n_clusters):
             cluster_points = X[labels == k]
             if len(cluster_points) > 0:
-                optimoids[k] = np.median(cluster_points, axis=0)
+                centers[k] = np.median(cluster_points, axis=0)
     elif cost_metric in ['euclidean', 'euclidean^1', 'L2']:
         for k in range(n_clusters):
             cluster_points = X[labels == k]
             if len(cluster_points) > 0:
-                optimoids[k] = weiszfeld(cluster_points, tolerance, max_steps)
+                centers[k] = weiszfeld(cluster_points, tolerance, max_steps)
     elif is_Lp(cost_metric) or is_euclidean_power(cost_metric):
+    # FIXME: For high values of n, the cost metric euclidean^n produces overflow errors. Users are welcome to contribute a fix (e.g. scaling max(abs()) of the data to 1)
+    # However, care should be taken to perform the inverse transform on all output values to avoid confusing the end user
         if is_Lp(cost_metric):
             p = int(cost_metric[1:])
             n = 1
@@ -77,16 +82,16 @@ def update_optimoids(X, labels, n_clusters, optimoids, cost_metric=None, toleran
                 grad = np.zeros(X.shape[1])
                 num_points = np.sum(labels == k)  # Number of points in cluster k
                 for x in X[labels == k]:
-                    distance = np.abs(optimoids[k] - x)
+                    distance = np.abs(centers[k] - x)
                     non_zero_indices = distance != 0
                     if np.any(non_zero_indices):  # Check if there are non-zero indices
-                        grad += n * np.sum(distance[non_zero_indices]**p, axis=-1)**(n/p - 1) * distance[non_zero_indices]**(p - 1) * np.sign(optimoids[k] - x)[non_zero_indices]
+                        grad += n * np.sum(distance[non_zero_indices]**p, axis=-1)**(n/p - 1) * distance[non_zero_indices]**(p - 1) * np.sign(centers[k] - x)[non_zero_indices]
                 grad[np.isnan(grad)] = 0  # Replace NaN values with zero
                 if np.allclose(grad, 0, atol=tolerance):
                     break
-                optimoids[k] -= descent_rate * grad / num_points
+                centers[k] -= descent_rate * grad / num_points
 
-    return optimoids
+    return centers
 
 
 def weiszfeld(X, tolerance, max_steps):
@@ -128,7 +133,7 @@ def weiszfeld(X, tolerance, max_steps):
 
 def plusplus(X, n_clusters, cost_metric=None, random_state=None, verbose=True):
     """
-    This function implements the k-means++ initialization method for the optimoids.
+    This function implements the k-means++ initialization method for the centers.
     This method aims to provide a better initialization than random initialization, which can lead to bad local optima.
     
     :param X: The input data.
@@ -136,7 +141,7 @@ def plusplus(X, n_clusters, cost_metric=None, random_state=None, verbose=True):
     :param cost_metric: The cost metric used for clustering.
     :param random_state: The seed for the random number generator.
     :param verbose: Whether to print warnings and suggestions.
-    :return: The initialized optimoids.
+    :return: The initialized centers.
     """
     if cost_metric is None:
         raise ValueError('No value for the argument "cost_metric" was received.')
@@ -148,42 +153,42 @@ def plusplus(X, n_clusters, cost_metric=None, random_state=None, verbose=True):
     else:
         np.random.seed(random_state)
     
-    optimoids = np.empty((n_clusters, X.shape[1]))
-    optimoids[0] = X[np.random.randint(X.shape[0])]  # Choose the first optimoid randomly
+    centers = np.empty((n_clusters, X.shape[1]))
+    centers[0] = X[np.random.randint(X.shape[0])]  # Choose the first center randomly
     for k in range(1, n_clusters):
-        distances = calculate_distances(X, cost_metric, optimoids[:k])
-        probabilities = np.min(distances, axis=1) / np.sum(np.min(distances, axis=1))  # Compute the probability of each point being chosen as the next optimoid
-        optimoids[k] = X[np.random.choice(X.shape[0], p=probabilities)]  # Choose the next optimoid randomly, with probabilities proportional to the distances from the previous optimoids
+        distances = compute_distances(X, cost_metric, centers[:k])
+        probabilities = np.min(distances, axis=1) / np.sum(np.min(distances, axis=1))  # Compute the probability of each point being chosen as the next center
+        centers[k] = X[np.random.choice(X.shape[0], p=probabilities)]  # Choose the next center randomly, with probabilities proportional to the distances from the previous centers
     
-    return optimoids
+    return centers
 
 
-def calculate_distances(X, cost_metric=None, optimoids=None):
+def compute_distances(X, cost_metric=None, centers=None):
     """
-    This function calculates the distances from each data point to each optimoid.
+    This function calculates the distances from each data point to each center.
     
     :param X: The input data.
     :param cost_metric: The cost metric used for clustering.
-    :param optimoids: The optimoids of the clusters.
-    :return: The distances from each data point to each optimoid.
+    :param centers: The centers of the clusters.
+    :return: The distances from each data point to each center.
     """
     if cost_metric is None:
         raise ValueError('No value for the argument "cost_metric" was received.')
-    if optimoids is None:
-        raise ValueError('No value for the argument "optimoids" was received.')
+    if centers is None:
+        raise ValueError('No value for the argument "centers" was received.')
     
     if cost_metric in ['squared_euclidean', 'euclidean^2']:
-        distances = np.linalg.norm(X[:, np.newaxis] - optimoids, axis=-1)**2
+        distances = np.linalg.norm(X[:, np.newaxis] - centers, axis=-1)**2
     elif cost_metric in ['manhattan', 'L1']:
-        distances = np.linalg.norm(X[:, np.newaxis] - optimoids, ord=1, axis=-1)
+        distances = np.linalg.norm(X[:, np.newaxis] - centers, ord=1, axis=-1)
     elif cost_metric in ['euclidean', 'euclidean^1', 'L2']:
-        distances = np.linalg.norm(X[:, np.newaxis] - optimoids, axis=-1)
+        distances = np.linalg.norm(X[:, np.newaxis] - centers, axis=-1)
     elif is_Lp(cost_metric):
         p = int(cost_metric[1:])
-        distances = np.linalg.norm(X[:, np.newaxis] - optimoids, ord=p, axis=-1)
+        distances = np.linalg.norm(X[:, np.newaxis] - centers, ord=p, axis=-1)
     elif is_euclidean_power(cost_metric):
         p = 2 # this exponent can be customized by end-users for non-euclidean distances
         n = int(cost_metric[10:])
-        distances = np.linalg.norm(X[:, np.newaxis] - optimoids, ord=p, axis=-1)**n
+        distances = np.linalg.norm(X[:, np.newaxis] - centers, ord=p, axis=-1)**n
         
     return distances
